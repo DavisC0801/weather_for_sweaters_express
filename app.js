170bcd75ee7e046ee1522488f0e6d12dfd2671a0
davisc0801 <davisc1014@gmail.com>
2019-08-13 17:14:25 -0600

    Remove unused routes and views

--- app.js
+++ app.js
@@ -2,10 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+var usersRouter = require('./routes/api/v1/users');
 var app = express();
 
 app.use(logger('dev'));
@@ -14,7 +11,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/v1/users', usersRouter);
 
 module.exports = app;
