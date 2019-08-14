6e5e752a5524a09c6b426f2c18703bc184c3394b
davisc0801 <davisc1014@gmail.com>
2019-08-13 18:47:28 -0600

    Add API key to user seed file

--- seeders/20190813235624-users-seeds.js
+++ seeders/20190813235624-users-seeds.js
@@ -4,13 +4,16 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', {
       email: 'user1@1.com',
-      password: 'password'
+      password: 'password',
+      apiKey: "12345"
     },{
       email: 'user2@2.com',
-      password: 'password2'
+      password: 'password2',
+      apiKey: "23456"
     },{
       email: 'user3@3.com',
-      password: 'password3'
+      password: 'password3',
+      apiKey: "34567"
     });
   },
 
