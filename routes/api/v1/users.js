c9232e926bd55deef429c92d17479077a770ce84
davisc0801 <davisc1014@gmail.com>
2019-08-14 16:03:16 -0600

    Add sad path for invalid info being sent

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -5,28 +5,34 @@ const router = express.Router();
 const user = require('../../../models').User;
 
 router.post('/', function(req, res) {
-  let existingUser = user.findOne({
-    where: {
-      email: req.body.email
-    }
-  });
-  if (req.body.password === req.body.password_confirmation && existingUser === null){
-    user.create({
-      email: req.body.email,
-      password: bcrypt.hashSync(req.body.password, 10),
-      apiKey: crypto.randomBytes(14).toString('hex')
-    })
-    .then(user => {
-      res.setHeader("Content-Type", "application/json");
-      res.status(201).send(JSON.stringify({api_key: user.apiKey}));
-    })
-    .catch(error => {
-      res.setHeader("Content-Type", "application/json");
-      res.status(500).send({error})
+  if (req.body.email !== undefined){
+    user.findOne({
+      where: {
+        email: req.body.email
+      }
+    }).then(existingUser => {
+      if (req.body.password === req.body.password_confirmation && existingUser === null){
+        user.create({
+          email: req.body.email,
+          password: bcrypt.hashSync(req.body.password, 10),
+          apiKey: crypto.randomBytes(14).toString('hex')
+        })
+        .then(user => {
+          res.setHeader("Content-Type", "application/json");
+          res.status(201).send(JSON.stringify({api_key: user.apiKey}));
+        })
+        .catch(error => {
+          res.setHeader("Content-Type", "application/json");
+          res.status(500).send(JSON.stringify({error}));
+        });
+      } else {
+        res.setHeader("Content-Type", "application/json");
+        res.status(406).send(JSON.stringify({error: "Invalid Email/Password Combination"}));
+      };
     });
   } else {
     res.setHeader("Content-Type", "application/json");
-    res.status(406).send("{ \n Invalid Email/Password Combination }")
+    res.status(406).send(JSON.stringify({error: "Invalid Email/Password Combination"}));
   };
 });
 
--- routes/api/v1/users.spec.js
+++ routes/api/v1/users.spec.js
@@ -8,6 +8,7 @@ describe('api', () => {
   });
 
   beforeEach(() => {
+    jest.setTimeout(1000);
     shell.exec('npx sequelize db:migrate');
   });
 
@@ -15,10 +16,49 @@ describe('api', () => {
     shell.exec('npx sequelize db:migrate:undo:all');
   });
 
+  afterAll(() => {
+    shell.exec('npx sequelize db:drop');
+  })
+
   describe('api v1 users create path', () => {
     test('it should respond to a post request', () => {
-      return request(app).post('/api/v1/users').then(response => {
-        expect(response.statusCode).toBe(201);
+      return request(app).post('/api/v1/users', (req, res) => {
+        req.send(JSON.stringify({
+          email: "test@test.com",
+          password: "password",
+          password_confirmation: "password"
+        }))
+        .then(response => {
+          expect(response.statusCode).toBe(201);
+        });
+      });
+    });
+
+    test('it should send an API key if valid info is sent', () => {
+      return request(app).post('/api/v1/users', (req, res) => {
+        req.send(JSON.stringify({
+          email: "test@test.com",
+          password: "password",
+          password_confirmation: "password"
+        }))
+      .then(response => {
+        expect(response.body.length).toEqual(1),
+        expect(Object.keys(response.body[0])).toContain('api_key')
+        });
+      });
+    });
+
+    test('it should send an error if invalid info is sent', () => {
+      return request(app).post('/api/v1/users', (req, res) => {
+        req.send(JSON.stringify({
+          email: "test@test.com",
+          password: "password",
+          password_confirmation: "notmypassword"
+        }))
+      .then(response => {
+        expect(response.body.length).toEqual(1),
+        expect(Object.keys(response.body[0])).toContain('error')
+        });
       });
     });
   });
