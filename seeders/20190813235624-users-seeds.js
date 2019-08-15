f373878a409c82fd1c9179545b9813f8ceeeab18
davisc0801 <davisc1014@gmail.com>
2019-08-15 08:42:52 -0600

    Add hashed passwords to seeder files

--- seeders/20190813235624-users-seeds.js
+++ seeders/20190813235624-users-seeds.js
@@ -2,19 +2,25 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Users', {
-      email: 'user1@1.com',
-      password: 'password',
-      apiKey: "12345"
+    return queryInterface.bulkInsert('Users', [{
+      email: 'test@test.com',
+      password: '$2b$10$beL6aXbGq8..THbAGDYB5.IQJsKkEI0MwmsOYi7TTt0pJwd2zSHq.',
+      apiKey: "12345",
+      createdAt: new Date(),
+      updatedAt: new Date()
     },{
-      email: 'user2@2.com',
-      password: 'password2',
-      apiKey: "23456"
+      email: 'user@user.com',
+      password: '$2b$10$beL6aXbGq8..THbAGDYB5.IQJsKkEI0MwmsOYi7TTt0pJwd2zSHq.',
+      apiKey: "23456",
+      createdAt: new Date(),
+      updatedAt: new Date()
     },{
-      email: 'user3@3.com',
-      password: 'password3',
-      apiKey: "34567"
-    });
+      email: 'admin@admin.com',
+      password: '$2b$10$beL6aXbGq8..THbAGDYB5.IQJsKkEI0MwmsOYi7TTt0pJwd2zSHq.',
+      apiKey: "34567",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }]);
   },
 
   down: (queryInterface, Sequelize) => {
