a9ea30e452b95c44bdf67bf3965eff20a0adb3d3
davisc0801 <davisc1014@gmail.com>
2019-08-18 14:12:15 -0600

    Add route to show favorites forecasts

--- routes/api/v1/favorites.js
+++ routes/api/v1/favorites.js
@@ -4,6 +4,7 @@ const fetch = require("node-fetch");
 const router = express.Router();
 const user = require('../../../models').User;
 const favorite = require('../../../models/').Favorite;
+const Forecast = require('../../../lib/forecast');
 
 router.post('/', function(req, res) {
   if (req.body.api_key !== undefined){
@@ -18,15 +19,29 @@ router.post('/', function(req, res) {
       .then(res => res.json())
       .then(data => {
         let coordinates = data["results"][0]["geometry"]["location"]
-        favorite.create({
+        favorite.findOrCreate({
+          where: {
+            location: req.body.location
+          },
           location: req.body.location,
           latitude: coordinates["lat"],
           longitude: coordinates["lng"],
           UserId: user.id
-        });
-        res.setHeader("Content-Type", "application/json");
-        res.status(200).send(JSON.stringify({message: `${req.body.location} has been added to your favorites`}));
         })
+        .then(([favorite, created]) => {
+          if (created) {
+            res.setHeader("Content-Type", "application/json");
+            res.status(200).send(JSON.stringify({message: `${req.body.location} has been added to your favorites`}));
+          } else {
+            res.setHeader("Content-Type", "application/json");
+            res.status(406).send(JSON.stringify({error: `${req.body.location} is already in your favorites`}));
+          }
+        })
+        .catch(error => {
+          res.setHeader("Content-Type", "application/json");
+          res.status(401).send(JSON.stringify({error: "Invalid Information"}));
+        });
+      })
       .catch(error => {
         res.setHeader("Content-Type", "application/json");
         res.status(401).send(JSON.stringify({error: "Invalid Information"}));
@@ -35,6 +50,46 @@ router.post('/', function(req, res) {
       res.setHeader("Content-Type", "application/json");
       res.status(401).send(JSON.stringify({error: "Invalid Information"}));
     }})
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(401).send(JSON.stringify({error: "Invalid Information"}));
+    });
+  } else {
+    res.setHeader("Content-Type", "application/json");
+    res.status(401).send(JSON.stringify({error: "Invalid Information"}));
+  };
+});
+
+router.get('/', function(req, res) {
+  if (req.body.api_key !== undefined){
+    favorite.findAll({
+      where: {
+        '$user.apiKey$': req.body.api_key
+      },
+      include: ['user']
+    }).then(favorites => {
+      if(favorites !== undefined){
+        let forecast = new Forecast()
+        favorites.forEach(function(city){
+          fetch(`https://api.darksky.net/forecast/${process.env.DARKSKY_API_KEY}/${city["dataValues"]["latitude"]},${city["dataValues"]["longitude"]}?exclude=minutely,hourly,daily`)
+          .then(res => res.json())
+          .then(data => {
+            forecast.addFavorite(data, city["dataValues"]["location"])
+          })
+          .catch(error => {
+            res.setHeader("Content-Type", "application/json");
+            res.status(401).send(JSON.stringify({error}));
+          });
+        })
+        setTimeout(function(){
+          res.setHeader("Content-Type", "application/json");
+          res.status(200).send(JSON.stringify(forecast.favoritesForecast()));
+        }, 500)
+      } else {
+        res.setHeader("Content-Type", "application/json");
+        res.status(401).send(JSON.stringify({error: "No favorites cities found"}));
+      }
+    })
     .catch(error => {
       res.setHeader("Content-Type", "application/json");
       res.status(401).send(JSON.stringify({error: "Invalid Information"}));
